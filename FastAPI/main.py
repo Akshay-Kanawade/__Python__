109cf8fbd4b9eca94c95d4fb2bc89fc74edf7105
Akshay_Kanawade <kanawade.akshay9@gmail.com>
2023-05-21 23:06:01 +0530

    worked on fastapi and python

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,3 +1,26 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get("/")
+def root():
+    """ Summary
+    create the get method for checking first fast api app route
+    Returns:
+        simple message
+    """
+    return {"Message":"Welcome in Fast API ",
+            "Doc string" : root.__doc__}
+    
+@app.get("/post") 
+def get_post():
+    """_summary_
+    Create a route to get all post
+
+    Returns:
+        _type_: list of post in form of dict
+    """
+    return {"Message":"list of all post"}
+
---
+++ Python/text.py
@@ -0,0 +1,5 @@
+a=5; print(a)
+
+
+
+for i in range(10): if(i%2==0)print(i*5);else print(i);
